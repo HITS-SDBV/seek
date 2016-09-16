61f41082dbb4296c97e2f626b6fcfd97c0202d9f
Nils Woetzel <nils.woetzel@h-its.org>
2016-09-16 13:20:43 +0000

    added test for PublicationAuthor split_full_name

--- test/unit/publication_author_test.rb
+++ test/unit/publication_author_test.rb
@@ -2,4 +2,23 @@ require 'test_helper'
 
 class PublicationAuthorTest < ActiveSupport::TestCase
 
+  test "split fullname" do
+    name1 = ["Shmoe"]
+    name2 = ["Joe", "Shmoe"]
+    name3 = ["Joe", "C.", "Shmoe"]
+
+    firstname1, lastname1 = PublicationAuthor.split_full_name name1.join(" ")
+    firstname2, lastname2 = PublicationAuthor.split_full_name name2.join(" ")
+    firstname3, lastname3 = PublicationAuthor.split_full_name name3.join(" ")
+
+    assert_equal "", firstname1
+    assert_equal name1[0], lastname1
+
+    assert_equal name2[0], firstname2
+    assert_equal name2[1], lastname2
+
+    assert_equal name3.slice(0,2).join(" "), firstname3
+    assert_equal name3[2], lastname3
+  end
+
 end
